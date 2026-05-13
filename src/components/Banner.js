b91e6c473d3de5178c2e266970efeff48d7e8eb0
Lateefat Obisesan <temilele06@gmail.com>
2026-05-13 14:34:44 -0500

    re-writhing the css

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import Button from "./Button";
 
 function Banner(props) {
     return (
-        <section>
+        <section className="hero-banner">
             <h2>{props.title}</h2>
             <p>{props.text}</p>
             <div>
