1f5add43deb2dbcd00ea278d0a54b9f36315c94f
Lateefat Obisesan <temilele06@gmail.com>
2026-05-13 15:05:25 -0500

    re-writing the css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
+import logo from '../image/lephinton-logo.png';
+
 function Header (props) {
     return (
         <header>
-            <img src="./lephinton-logo.png" alt="Lephinton Crochet Logo" className="logo"/>
+            <img src={logo} alt="Lephinton Crochet Logo" className="logo"/>
             <nav>
                 <ul>
                    {props.navLinks.map((link, index) => (
